893c5ba841a01048bca90c672d3c0068f26013ed
jennifer <jennifer880719@gmail.com>
2025-07-26 14:36:06 +0800

    修正爬蟲程式_v1

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
-import datetime as dt
-print("Hello World!")
-print("今天日期:", dt.datetime.today().strftime("%Y/%m/%d"))
+# coding: utf-8
+import requests
+import pandas as pd
+import json
+url = "https://api.cnyes.com/media/api/v1/newslist/category/headline" # 新聞連結 # 連結問號前面是url，後面是參數
+payload = {"page":2, 
+           "limit":30, 
+           "isCategoryHeadline":1, 
+           "startAt":1752645083, 
+           "endAt":1753509083} # 參數
+res = requests.get(url, params = payload) # 連線鉅亨網
+jd = json.loads(res.text) # 解析json轉成dict
+df = pd.DataFrame(jd['items']['data']) # 取出新聞資料
+df = df[['newsId', 'title', 'summary']] # 取出特定欄位
+df['link'] = df['newsId'].apply(lambda x: 'https://m.cnyes.com/news/id/' + str(x)) # 建立連結
+df.to_csv('news.csv', encoding='utf-8-sig')
+print(df)
