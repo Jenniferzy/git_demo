0d114b81c6d0ec7adea14b8d1da666fa757772d4
jennifer <jennifer880719@gmail.com>
2025-07-26 16:23:03 +0800

    變更日期格式

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 # coding: utf-8
 import requests
 import pandas as pd
+import datetime as dt
 import json
 url = "https://api.cnyes.com/media/api/v1/newslist/category/headline" # 新聞連結 # 連結問號前面是url，後面是參數
 payload = {"page":2, 
            "limit":30, 
            "isCategoryHeadline":1, 
-           "startAt":1752645083, 
-           "endAt":1753509083} # 參數
+           "startAt":int((dt.datetime.today() - dt.timedelta(days = 11)).timestamp()), 
+           "endAt":int(dt.datetime.today().timestamp())} # 參數
 res = requests.get(url, params = payload) # 連線鉅亨網
 jd = json.loads(res.text) # 解析json轉成dict
 df = pd.DataFrame(jd['items']['data']) # 取出新聞資料
